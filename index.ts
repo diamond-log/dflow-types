5570a5ad793077c3a77ab62b7a0e7f96b04d3e89
frednunesjr <fred@multipixel.com.br>
2024-06-18 15:31:44 -0300

    export types

--- index.ts
+++ index.ts
@@ -1,31 +1,31 @@
-interface Workspace {
+export interface Workspace {
 	users: Users[];
 }
 
-interface TaskGroup {
+export interface TaskGroup {
 	id: string;
 	name: string;
 	order: number;
 	tasks: Task[];
 }
 
-interface Task {
+export interface Task {
 	id: string;
 	owners: Users[];
 	teams: Team[];
 }
 
-interface Project {
+export interface Project {
 	id: string;
 	teams: Team[];
 	tasksGroups: TaskGroup[];
 }
 
-interface Team {
+export interface Team {
 	id: string;
 	users: Users[];
 }
 
-interface Users {
+export interface Users {
 	id: string;
 }
