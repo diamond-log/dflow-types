d7ee94e5af3aeb1a6880d11096c0f1c8a6591276
AlegSandrin <alexsandrourbano157@gmail.com>
2024-07-25 12:32:02 -0300

    Campo "files" na interface Project

--- interfaces/Project.ts
+++ interfaces/Project.ts
@@ -3,15 +3,17 @@ import { Team } from "./Team";
 import { TaskGroup } from "./TaskGroup";
 import { Icons } from "../types/Icons";
 import { Metadata } from "./Metadata";
+import { File } from "./File";
 
 export interface Project {
 	uid: string;
     icon: Icons;
-    metadata: Metadata;
+    metadata: Metadata; 
     name: string;
 	taskGroups: TaskGroup[];
     teams: Team[];
     members: ProjectMember[];
     privacy: "public" | "private";
     description: string;
+    files: File[];
 }
